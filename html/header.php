8c7b48507dc85ac6d789dc6fb73e9f80926e900f
Hikaru Aihara <communist051203@gmail.com>
2023-01-16 15:06:08 +0900

    本日分の修正

--- html/header.php
+++ html/header.php
@@ -1,35 +1,22 @@
-    <div class="container item">
-      <h1>辞書一覧(印欧語)</h1>
-        <section class="row">
-          <div class="col-md-1 mb-0">
-            <a class="btn btn-primary" href="latin/latin_index.php">ラテン語</a>
-          </div>
-          <div class="col-md-1">
-            <a class="btn btn-primary" href="latin/dictionary_latin_adjective.php">形容詞</a>
-          </div>
-          <div class="col-md-1">
-            <a class="btn btn-primary" href="latin/dictionary_latin_verb.php">動詞</a>
-          </div>
-          <div class="col-md-1">
-            <a class="btn btn-primary" href="latin/practice_latin_noun.php">問題集<br>(名詞)</a>
-          </div>
-          <div class="col-md-1">
-            <a class="btn btn-primary" href="latin/practice_latin_adjective.php">問題集<br>(形容詞)</a>
-          </div>
-          <div class="col-md-1">
-            <a class="btn btn-primary" href="latin/practice_latin_verb.php">問題集<br>(動詞)</a>
-          </div>
-          <div class="col-md-1">
-            <a class="btn btn-primary" href="latin/practice_latin_noun2.php">問題集<br>(名詞)2</a>
-          </div>
-          <div class="col-md-1">
-            <a class="btn btn-primary" href="latin/practice_latin_adjective2.php">問題集<br>(形容詞)2</a>
-          </div>
-          <div class="col-md-1">
-            <a class="btn btn-primary" href="latin/practice_latin_verb2.php">問題集<br>(動詞)2</a>
-          </div>
-        </section>
+<div class="container item">
+  <h1>辞書一覧(古典語)</h1>
+  <section class="row">
+    <div class="col-md-1 mb-0">
+      <a class="btn btn-primary" href="latin/latin_index.php">ラテン語</a>
     </div>
-    <div class="container item">
-      <h1>辞書一覧(非印欧語)</h1>
-    </div>  
+    <div class="col-md-1">
+      <a class="btn btn-primary" href="sanskrit/sanskrit_index.php">梵語</a>
+    </div>
+    <div class="col-md-1">
+      <a class="btn btn-primary" href="koine/koine_index.php">ギリシア語</a>
+    </div>
+  </section>
+</div>
+<div class="container item">
+  <h1>辞書一覧(現代語)</h1>
+  <section class="row">
+  <div class="col-md-1 mb-0">
+      <a class="btn btn-primary" href="polish/polish_index.php">ポーランド語</a>
+    </div>
+  </section>
+</div>  
